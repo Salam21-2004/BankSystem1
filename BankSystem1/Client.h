22473d397baeb79c5fd7d71627fae8558bb4350d
Salam <salam409435922@gmail.com>
2026-03-05 13:32:26 +0200

    client class

--- BankSystem1/Client.h
+++ BankSystem1/Client.h
@@ -1,1 +1,74 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include "Person.h"
+#include "Validation.h"
+
+using namespace std;
+
+class client : public Person {
+protected:
+    double balance;
+
+public:
+    client() : Person() {
+        balance = 1500;
+    }
+
+    client(int id, string name, string password, double balance)
+        : Person(id, name, password) {
+        setBalance(balance);
+    }
+
+    void setBalance(double balance) {
+        if (Validation::set_balance(balance)) {
+            this->balance = balance;
+        }
+    }
+
+    double getBalance() const {
+        return balance;
+    }
+
+    void deposit(double amount) {
+        if (amount <= 0) {
+            cout << "invalid deposit amount\n";
+            return;
+        }
+        balance += amount;
+    }
+
+    void withdraw(double amount) {
+        if (amount <= 0) {
+            cout << "invalid withdraw amount\n";
+            return;
+        }
+        if (balance - amount < 1500) {
+            cout << "Cannot withdraw, minimum balance is 1500\n";
+            return;
+        }
+        balance -= amount;
+    }
+
+    void transferTo(double amount, client& recipient) {
+        if (amount <= 0) {
+            cout << "invalid transfer amount\n";
+            return;
+        }
+        if (balance - amount < 1500) {
+            cout << "Cannot transfer, minimum balance is 1500\n";
+            return;
+        }
+        balance -= amount;
+        recipient.balance += amount;
+    }
+
+    void checkBalance() const {
+        cout << "Balance = " << balance << endl;
+    }
+
+    void display() const override {
+        Person::display();
+        cout << "Balance = " << balance << endl;
+    }
+};
